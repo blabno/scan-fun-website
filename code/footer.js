8a6fdfa3b7beabf87d75853a1a555e4bf0d2fb81
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2019-12-22 11:38:19 +0100

    Mobile app links in footer

---
+++ code/footer.js
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react';
+import {
+  COLLECTOR_APP_ANDROID_URL,
+  COLLECTOR_APP_IOS_URL,
+  DISTRIBUTOR_APP_ANDROID_URL,
+  DISTRIBUTOR_APP_IOS_URL,
+  MANAGER_APP_ANDROID_URL,
+  MANAGER_APP_IOS_URL
+} from './config';
+import Row from './row';
+
+
+const Footer = ({ _body, _ID, _relativeURL }) => (
+  <Fragment>
+    <Row>
+      <ul className={'list-unstyled'}>
+        <li>
+          Aplikacja dla zbierających kupony:
+          <ul>
+            <li><a href={COLLECTOR_APP_ANDROID_URL}>Android</a></li>
+            <li><a href={COLLECTOR_APP_IOS_URL}>iOS</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul className={'list-unstyled'}>
+        <li>
+          Aplikacja dla biznesu:
+          <ul>
+            <li><a href={MANAGER_APP_ANDROID_URL}>Android</a></li>
+            <li><a href={MANAGER_APP_IOS_URL}>iOS</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul className={'list-unstyled'}>
+        <li>
+          Aplikacja Dystrybutor:
+          <ul>
+            <li><a href={DISTRIBUTOR_APP_ANDROID_URL}>Android</a></li>
+            <li><a href={DISTRIBUTOR_APP_IOS_URL}>iOS</a></li>
+          </ul>
+        </li>
+      </ul>
+    </Row>
+    <Row>
+      <div className={'copyright'}>
+        {_body}
+      </div>
+    </Row>
+  </Fragment>
+);
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
--- content/_shared/footer.md
+++ content/_shared/footer.md
@@ -1,4 +1,4 @@
 ---
-layout: row 
+layout: footer 
 ---
 Copyright Labnoratory Ltd @2019
